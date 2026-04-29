b55158e61a864e722da6ac878f0f028ec2713e35
prezydent80 <tpolluke@gmail.com>
2026-04-29 10:54:52 +0200

    Update wyznacznik_1.c

--- wyznacznik_1.c
+++ wyznacznik_1.c
@@ -6,17 +6,17 @@ float tab_2[4];
 int size = (sizeof(tab_1) / sizeof(tab_1[0]));
 
 int main() {
-	int liczba1;
-	int liczba2;
-	int liczbaA;
-	int liczbaB;
+	float wyznacznik;
+	int index;
 
 	for (int x = 0, index = 0; x < 2; x++) {
 		for (int y = 0; y < 2; y++, index++) {
-			if (y = 0) {
-				liczba1 = tab_1[x][y];
-			}
+			tab_2[index] = tab_1[x][y];
+			printf("%f \n", tab_2[index]);
 		}
 	}
+
+	wyznacznik = (tab_2[0] * tab_2[3]) - (tab_2[1] * tab_2[2]);
+	printf("wyznacznik macierzy jest rowny: %f", wyznacznik);
 
 }
